9c55ddc6bb278d44c5d868e8c6f5a38cfdc3384a
SantinoG07 <santino.gennuso.m@gmail.com>
2024-11-10 10:11:11 -0300

    :recycle: windSpeed

---
+++ components/windspeed.jsx
@@ -0,0 +1,81 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet } from "react-native";
+import axios from 'axios';
+
+const Windir = () => {
+  const [windSpeed, setWindSpeed] = useState(null);
+
+  useEffect(() => {
+    const getWeatherData = async () => {
+      try {
+        const apiKey = 'e3ca4788ae9a4b04b5a170108241810';  
+        const location = 'Belgrado';
+        const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`;
+
+        console.log("Haciendo solicitud a la API...");
+
+        const response = await axios.get(url);
+
+        const windSpeed = response.data.current.wind_kph;
+
+        setWindSpeed(windSpeed);
+
+        console.log("Velocidad del viento:", windSpeed);
+      } catch (error) {
+        console.error("Error en la API:", error);
+      }
+    };
+
+    getWeatherData();
+  }, []);
+
+  
+
+  return (
+    <View style={{ backgroundColor: '#3f3f3f', flex: 1, paddingTop: 20 }}>
+      <StatusBar style="light" />
+
+      {windSpeed ? (
+        <View style={styles.infoContainer}>
+          <Text style={styles.title}>Velocidad del viento</Text>
+          
+          <Text style={styles.text}>{windSpeed}</Text>
+          <Text style={styles.km}>KM/H</Text>
+        </View>
+      ) : (
+        <Text style={styles.text}>Cargando información del viento...</Text>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 15,
+    color: '#1e6fc7',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  km:{
+    color:'#868686',
+    fontSize:20,
+  },
+  text: {
+    fontSize: 45,
+    color: 'white',
+    textAlign: 'center',
+  },
+  infoContainer: {
+    backgroundColor:'#393939',
+    paddingTop:16,
+    paddingBottom:19,
+    borderRadius:22,
+    height:190,
+    width:180,
+    marginTop: 20,
+    alignItems: 'center',
+  },
+});
+
+export default Windir;
--- pages/avanzado.jsx
+++ pages/avanzado.jsx
@@ -3,12 +3,14 @@ import { View, Text } from "react-native";
 import BarGraphics from "../components/bargraphics"
 import BarGraphics1 from "../components/bargraphics1"
 import Windir from "../components/windir"
+import WindSpeed from "../components/windspeed.jsx"
 
 const Avanzado = () => {
   return (
     <View style={{backgroundColor:'#3f3f3f', flex:1}}>
       <StatusBar style="light" />
       <Windir/>
+      <WindSpeed/>
     </View>
   );
 };
